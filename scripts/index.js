8b375ea88cc75ed70349aa4887e4f0d50829478c
Sander <sander.smeman@jdriven.com>
2019-01-12 00:05:01 +0100

    Convert blog to hexo (#3)
    * Remove jekyll related files
    
    * Create Hexo default site
    
    * Add clean-blog theme
    
    * Update location of post
    
    * Use asciidoc for rendering, styling complete
    
    * Update travis config
    
    * Run travis on to-hexo branch
    
    * Upgrade node for travis to 8.15.0

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const asciidoc = require('./lib/asciidoc.js')
+
+hexo.extend.renderer.register('ad', 'html', asciidoc, true)
+hexo.extend.renderer.register('adoc', 'html', asciidoc, true)
+hexo.extend.renderer.register('asciidoc', 'html', asciidoc, true)
+
+// hexo.extend.helper.register('structured_data', require('./lib/structured_data.js'));
