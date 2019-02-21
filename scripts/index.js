5d746bf6eaae200a0f28f3efdd24577fa5a78c34
Sander Smeman <sander.smeman@gmail.com>
2019-02-21 22:20:13 +0100

    Add other page templates

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,16 @@ hexo.extend.renderer.register('ad', 'html', asciidoc, true);
 hexo.extend.renderer.register('adoc', 'html', asciidoc, true);
 hexo.extend.renderer.register('asciidoc', 'html', asciidoc, true);
 
-// hexo.extend.helper.register('structured_data', require('./lib/structured_data.js'));
+hexo.extend.helper.register('structured_data', require('./lib/structured_data.js'));
+
+Array.prototype.shuffle = function() {
+    var i = this.length, j, temp;
+    if ( i == 0 ) return this;
+    while ( --i ) {
+        j = Math.floor( Math.random() * ( i + 1 ) );
+        temp = this[i];
+        this[i] = this[j];
+        this[j] = temp;
+    }
+    return this;
+}
