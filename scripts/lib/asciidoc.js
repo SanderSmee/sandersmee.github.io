2ff939f596b534b93b1ed3388f88cae31f5f29f6
Sander Smeman <sander.smeman@jdriven.com>
2019-06-11 13:34:51 +0200

    Improve output html by using html5s backend for asciidoctor

--- scripts/lib/asciidoc.js
+++ scripts/lib/asciidoc.js
@@ -1,10 +1,15 @@
 'use strict'
 
 const asciidoctor = require('asciidoctor')();
+const asciidoctorHtml5s = require('asciidoctor-html5s');
 // const path = require('path');
+
+// Register the HTML5s converter and supporting extension.
+asciidoctorHtml5s.register();
 
 const opts = {
     safe: 'safe',
+    backend: 'html5s',
     attributes: {
         doctype: 'article',
         showtitle: false,
